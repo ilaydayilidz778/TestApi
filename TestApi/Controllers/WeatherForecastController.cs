73b3ca413ab951fb8426c59b9672ee790a6e3b06
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-12-22 23:54:38 +0300

    Summaries Clollection has been edited.

--- TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Blazing", "Frigid"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Blazing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
